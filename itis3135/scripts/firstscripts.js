b5613d6da0ae1badedd6681b54f14f22f6fccd5f
bgorman3 <bgorman3@uncc.edu>
2023-10-08 03:57:05 -0400

    lol

--- itis3135/scripts/firstscripts.js
+++ itis3135/scripts/firstscripts.js
@@ -2,15 +2,19 @@
 function displayDateTime() {
     const now = new Date();
     const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][now.getDay()];
-    const date = now.getDate();
-    const month = now.toLocaleString('default', { month: 'long' });
-    const year = now.getFullYear();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
     
-    const dateTimeString = `Today is ${hours}:${minutes < 10 ? '0' : ''}${minutes} on ${dayOfWeek}, ${date} ${month}, ${year}`;
-    document.getElementById("heading").textContent = dateTimeString;
+    const month = now.toLocaleString('default', { month: 'long' });
+    const date = now.getDate();
+    const year = now.getFullYear();
+    const hours = now.getHours() % 12 || 12; // Get 12-hour format
+    const minutes = now.getMinutes();
+    const amOrPm = now.getHours() >= 12 ? 'PM' : 'AM'; // Determine AM or PM
+    
+    const dateTimeString = `Today is ${hours}:${minutes < 10 ? '0' : ''}${minutes} ${amOrPm} on ${dayOfWeek}, ${month} ${date}, ${year}`;
+    document.getElementById("dateTime").textContent = dateTimeString;
+   
 }
+
 
 // Function to greet the user
 function greetUser() {
@@ -37,17 +41,19 @@ function getPolygonName() {
 }
 
 // Functions for Animal Brand Company
-function provideInsult() {
+function function1() {
     alert("Function 1: We provide the quirkiest animal-themed merchandise!");
 }
 
-function takeSumOfTwoNumbers() {
+function function2() {
     alert("Function 2: Our animals can predict the weather. Ask us for today's forecast!");
 }
 
-function calculateTax() {
+function function3() {
     alert("Function 3: We offer pet psychic readings. Find out what your pet is thinking!");
 }
-
+function function4(){
+    alert("We cant wait for your buisness for the GreedyGoblins")
+}
 // Call displayDateTime function when the page loads
 displayDateTime();
