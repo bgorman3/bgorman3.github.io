8fc183de91239bfdc056d7af0053a75f2c93be3a
bgorman3 <bgorman3@uncc.edu>
2023-12-03 23:11:26 -0500

    slideshowdone

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,23 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function plusSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    const captions = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+    captions.innerHTML = slides[slideIndex - 1].alt;
+}
