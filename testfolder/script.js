cb90a4a252521831c0b7967d48c7ff02f97f3df7
bgorman3 <bgorman3@uncc.edu>
2023-12-06 01:45:33 -0500

    lol

--- testfolder/script.js
+++ testfolder/script.js
@@ -30,3 +30,26 @@ function displayImages(imageArray) {
 function navigateToPage(page) {
   window.location.href = page;
 }
+
+$(document).ready(function(){
+  $('.tooltip-trigger').hover(
+      function() {
+          var tooltipText = $(this).attr('data-tooltip');
+          $('<div class="tooltip"></div>').text(tooltipText).appendTo($(this).parent()).fadeIn('fast');
+      },
+      function() {
+          $(this).parent().find('.tooltip').remove();
+      }
+  ).mousemove(function(e) {
+      var xOffset = 10;
+      var yOffset = 10 + ($(this).height() / 2); // Adjust the yOffset based on half of the image height
+      var mousex = e.pageX + xOffset;
+      var mousey = e.pageY + yOffset;
+      $(this).parent().find('.tooltip').css({ top: mousey, left: mousex });
+  });
+});
+$(document).ready(function(){
+  // Example: Initialize a jQuery UI widget (e.g., datepicker)
+  $('#datepicker').datepicker();
+});
+
