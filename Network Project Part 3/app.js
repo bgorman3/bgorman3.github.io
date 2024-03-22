e705a4aea872a0d567a063b22b371ee4826830b8
bgorman3 <bgorman3@uncc.edu>
2024-03-22 03:34:16 -0400

    start of project 3

---
+++ Network Project Part 3/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const userRoutes = require('./routes/UserRoutes');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+let port = 4000;
+let host = 'localhost';
+let url = 'mongodb+srv://bgorman3:joelewis88@cluster0.feevi6q.mongodb.net/nbda-project3';
+
+app.set('view engine', 'ejs');
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, host, () => {
+      console.log(`Server is running on http://${host}:${port}`);
+    });
+  })
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.use('/', userRoutes);
+
+app.use((req, res, next) => {
+  const err = new Error(`The server cannot locate ${req.url}`);
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(status).render('error', { error: { status, message } });
+});
---
+++ Network Project Part 3/controllers/itemsController.js
@@ -0,0 +1,294 @@
+const itemsModel = require('../models/itemModel');
+const fs = require('fs');
+const path = require('path');
+
+
+exports.getAllItems = (req, res, next) => {
+    itemsModel.find()
+        .then(allItems => {
+            if (allItems.length === 0) {
+                console.log('No items found in the database');
+            } else {
+                // Sort items by price in ascending order
+                allItems.sort((a, b) => a.price - b.price);
+                console.log('Items received from the database:', allItems);
+            }
+            // Render view with data
+            res.render('items', { items: allItems });
+        })
+        .catch(err => {
+            console.error('Error fetching items from the database:', err);
+            next(err);
+        });
+};
+
+exports.searchItems = (req, res) => {
+    const searchTerm = req.query.term.toLowerCase();
+    const searchResults = itemsModel.searchItems(searchTerm);
+    if (searchResults && searchResults.length >  0) {
+        res.render('searchResults', { results: searchResults });
+    } else {
+        // Handle the case where no results are found
+        res.status(404).render('error', { message: 'No items found' });
+    }
+};
+
+
+
+exports.getItemDetails = (req, res) => {
+    const itemId = parseInt(req.params.id);
+    console.log('Requested Item ID:', itemId);
+    const item = itemsModel.getItemById(itemId);
+    console.log('Retrieved Item:', item);
+    if (item) {
+        res.render('item', { item: item }); // Render the 'item' view instead of 'searchResults'
+    } else {
+        res.status(404).render('error', { message: 'Item not found' });
+    }
+};
+
+
+
+
+        
+
+exports.createItem = (req, res, next) => {
+    console.log('Form data received:', req.body); // Log the received form data
+
+    // Ensure that req.body is defined and contains the expected properties
+    if (req.body && req.body.title) {
+        // Access form data from req.body
+        const newItem = new itemsModel({
+            title: req.body.title,
+            seller: req.body.seller,
+            condition: req.body.condition,
+            price: parseFloat(req.body.price),
+            details: req.body.details,
+            image: '/public/images/car1.png', // Default image path or update it as needed
+            totalOffers: 0,
+            active: true
+        });
+
+        if (req.file) {
+            newItem.image = `/images/${req.file.filename}`;
+        }
+
+        // Save the new item to the database
+        newItem.save()
+            .then((item) => {
+                console.log(item);
+                // Redirect to the item listing page
+                res.redirect('items');
+            })
+            .catch(err => {
+                if(err.name === 'ValidationError') {
+                    err.status = 400;
+                }
+                next(err);
+            });
+    } else {
+        // Handle the case when form data is missing or incorrect
+        res.status(400).send('Invalid form data');
+    }
+};
+
+
+exports.editItemView = (req, res) => {
+    // render edit view
+    const itemId = parseInt(req.params.id);
+    const item = itemsModel.getItemById(itemId);
+    if (item) {
+        res.render('edit', { item: item });
+    } else {
+        // Respond with 404 Not Found if item is not found
+        res.status(404).render('error', { message: 'Item not found' });
+    }
+};
+exports.editItem = (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const updatedFields = req.body;
+    const currentItem = itemsModel.getItemById(itemId);
+   
+    // Merge updated fields into the current item
+    const updatedItem = { ...currentItem, ...updatedFields };
+    updatedItem.price = parseInt(updatedItem.price);
+
+    if (req.file) {
+        updatedItem.image = `/images/${req.file.filename}`;
+    }
+
+    const isUpdated = itemsModel.updateItem(itemId, updatedItem);
+
+    if (isUpdated) {
+        // Redirect to the item details page
+        return res.redirect(`/items/${itemId}`);
+    } else {
+        // Handle error, item not found
+        res.status(404).render('error', { message: 'Item not found' });
+    }
+};
+
+
+exports.deleteItem = (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const isDeleted = itemsModel.deleteItem(itemId);
+    if (isDeleted) {
+        // Redirect to the item listing page
+        res.redirect('items');
+    } else {
+        // Handle error, item not found
+        res.status(404).render('error', { message: 'Item not found' });   
+     }
+};
+
+exports.renderSellPage = (req, res) => {
+    // Render the "Sell" page
+    res.render('new');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*first to get rid o f 
+
+exports.updateItem = (req, res, next) => {
+    const itemId = req.params.itemId;
+    const updatedItem = req.body;
+
+    if (auctionItems.updateById(itemId, updatedItem)) {
+        // Redirect to the items page after successful update
+        res.redirect('/items');
+    } else {
+        // Handle error (e.g., item not found)
+        const err = new Error('Cannot find an item with id ' + itemId);
+        err.status =  404;
+        next(err);
+    }
+};
+
+exports.editItem = (req, res) => {
+    const itemId = req.params.itemId;
+    const item = auctionItems.findById(itemId);
+
+    if (!item) {
+        res.status(404).send('Item not found');
+        return;
+    }
+
+    res.render('pages/edit', { item: item });
+};
+
+
+
+/*exports.edit = (req, res, next) => {
+    let itemId = req.params.itemId;
+    let item = model.findById(itemId);
+    if (item) {
+        res.render('pages/edit', {item: item});
+    } else{
+        let err = new Error('Cannot find a item with id ' + itemId);
+        err.status = 404;
+        next(err);
+    }
+};
+
+exports.updateItem = (req, res, next) => {
+    const itemId = req.params.itemId;
+    const updatedItem = req.body;
+
+    if (auctionItems.updateById(itemId, updatedItem)) {
+        // Sort items after updating
+        auctionItems.auctionItems.sort((a, b) => a.price - b.price);
+        // Redirect to the items page after successful update
+        res.redirect('/items');
+    } else {
+        // Handle error (e.g., item not found)
+        const err = new Error('Cannot find an item with id ' + itemId);
+        err.status =  404;
+        next(err);
+    }
+};
+
+*/ /*second
+exports.deleteItem = (req, res) => {
+    const itemId = req.params.itemId;
+    const index = auctionItems.findIndex(item => item.id === itemId);
+    if (index !== -1) {
+        auctionItems.splice(index,   1);
+        res.redirect('/items'); // Redirect to items page after deletion
+    } else {
+        
+        res.status(404).send('Item not found');
+    }
+};
+
+exports.renderItemsPage = (req, res) => {
+    const sortBy = req.query.sortBy;
+    let sortedItems = [...auctionItems.find()]; // Retrieve all items
+
+    if (sortBy === 'price') {
+        sortedItems.sort((a, b) => a.price - b.price);
+    } else if (sortBy === 'dateTaken') {
+        sortedItems.sort((a, b) => new Date(a.dateTaken) - new Date(b.dateTaken));
+    }
+
+    res.render('pages/items', { items: sortedItems });
+};
+
+exports.renderItemPage = (req, res) => {
+    const itemId = req.params.itemId;
+    const item = auctionItems.find(item => item.id.toString() === itemId);
+
+    if (!item) {
+        res.status(404).send('Item not found');
+        return;
+    }
+
+    res.render('pages/item', { item: item });
+
+   
+};
+exports.renderNewItemForm = (req, res) => {
+    res.render('pages/new-item');
+};
+exports.addItem = (req, res) => {
+    const newItem = req.body;
+
+    // Validate newItem here if needed
+
+    auctionItems.save(newItem); // Assuming auctionItems is imported correctly
+
+    res.redirect('/items'); // Redirect to the items page after adding the new item
+};
+
+/*
+exports.renderItemsPage = (req, res) => {
+    // Assuming auctionItems is imported correctly from your model
+    res.render('pages/items', { items: auctionItems.auctionItems });
+};
+*/
